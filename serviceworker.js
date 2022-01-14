efc6db194f3f4cd18d58d4323f2209e83c44d23d
brein <reinbeumer@gmail.com>
2022-01-14 02:13:35 +0100

    tried out PWA

---
+++ serviceworker.js
@@ -0,0 +1,18 @@
+const someProjects = 'some-projects-1';
+const assets = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.js',
+  '/img/2022-01-13 at 21.22.47 - Black Bandicoot.webm',
+  '/img/2022-01-13 at 21.30.05 - Olive Hedgehog.webm',
+  '/img/2022-01-13 at 21.39.28 - Silver Chimpanzee.webm',
+];
+
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(someProjects).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
